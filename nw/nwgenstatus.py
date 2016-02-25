eb292121aa2c1334ab8a4276c20c3f63de1c9ccf
Arjun Venkatraman <arjun@mojolab.org>
2016-02-25 12:11:14 +0530

    Updated

--- nw/nwgenstatus.py
+++ nw/nwgenstatus.py
@@ -7,9 +7,13 @@ if __name__=="__main__":
 	iplinks=os.popen("ip link show | grep 'state UP'").read().strip().split("\n")
 	status['ips']=[]
 	for link in iplinks:
+		dictionary={}
 		iface=link.split(": ")[1]
-		print iface,os.popen("ip addr show %s | grep 'inet '" %iface).read().strip().split(" ")[1].split("/")[0]
-		
+		ipaddr=os.popen("ip addr show %s | grep 'inet '" %iface).read().strip().split(" ")[1].split("/")[0]
+		print iface,ipaddr
+		dictionary['iface']=iface
+		dictionary['ipaddr']=ipaddr
+		status['ips'].append(dictionary)
 	f=open("status","w")
 	f.write(json.dumps(status))
 	f.close()
