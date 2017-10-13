5ab1ccd8cd01e653c5bd3c2fc7cfc45ac8d6861f
root <root@localhost.localdomain>
2017-10-13 18:46:30 +0530

    Updated mailer for modem balance

--- tools/mailmodembalance.py
+++ tools/mailmodembalance.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python
 
-import sys
+import sys,os
+from datetime import *
 
 sys.path.append("/opt/mojomailman/mojomail")
 from mojomail import *
 if __name__=="__main__":
-    mailconfig="/opt/mail.conf"
+    mailconfig="/opt/cowmail/mail.conf"
     mailer=MojoMailer(mailconfig)
-    mailer.logintoinmail()
+    #mailer.logintoinmail()
     messager=MojoMessager(mailconfig)
-    bodydict['
+    subdict=messager.getsubdict()
+    bodydict=messager.getbodydict()
+    timestamp=datetime.datetime.now()
+    print timestamp
+    subdict['TIMESTAMP']=timestamp.strftime("%Y-%b-%d %H:%M:%S")
+    subdict['MOJOMAIL']="COWHerd Report"
+    subdict['TYPE']="BalanceUpdate"
+    bodydict['CONTENT']=os.popen("fortune").read().strip()
+    msg=messager.composemessage(mailer.outusername,subdict,bodydict,"")
+    mailer.sendmsg(msg)
+				
