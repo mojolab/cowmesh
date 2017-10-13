68a73f2bb2112339372b1846133aa5ca4bc958ac
root <root@localhost.localdomain>
2017-10-13 19:18:28 +0530

    Playing with different time values to get the USSD for balance back

--- tools/checkmodembal.py
+++ tools/checkmodembal.py
@@ -4,8 +4,9 @@ ser=Serial("/dev/ttyUSB1",19200,timeout=1)
 if ser.inWaiting() > 0:
     ser.flushInput()
 ser.write('AT+CUSD=1,"*125#",15\r\n')
-time.sleep(3)
-msg=ser.read(size=1024)
+time.sleep(8)
+msg=ser.read(size=4096)
+print msg
 lines=msg.split("\n")
 for line in lines:
 	if "MB" in line:
