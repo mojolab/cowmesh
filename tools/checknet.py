47011b743d5d7dd9202dce4634813e835e0af567
Arjun Venkatraman <arjun@mojolab.org>
2017-10-30 17:23:02 +0530

    Indentation fix

--- tools/checknet.py
+++ tools/checknet.py
@@ -5,11 +5,11 @@ from libcowherdutils import *
 
 if __name__=="__main__":
 	logging.basicConfig(filename='/opt/cowherd.log', level=logging.DEBUG,format='%(asctime)s:%(levelname)s: %(message)s')
-    config=ConfigParser.ConfigParser()
-    config.read("/opt/cowherd.conf")
-    primary=config.get("ISP","primary")
-    secondary=config.get("ISP","secondary")
-    if not internet():
+	config=ConfigParser.ConfigParser()
+	config.read("/opt/cowherd.conf")
+	primary=config.get("ISP","primary")
+	secondary=config.get("ISP","secondary")
+	if not internet():
 		logging.info("Attempting restart of primary media")
         startmedia(primary)
     time.sleep(60)
