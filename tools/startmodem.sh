4aa9d3a4a852bb6e456b54e144e0cecb02c0823f
Arjun Venkatraman <arjun@mojolab.org>
2017-10-30 17:30:08 +0530

    Added nohup to modem start script

--- tools/startmodem.sh
+++ tools/startmodem.sh
@@ -2,7 +2,7 @@
 sudo ip route del default
 sudo killall wvdial
 sleep 20
-sudo wvdial &
+nohup sudo wvdial &
 sleep 30
 sudo sh /opt/cowmesh/tools/ishare.sh ppp0
 sudo echo "nameserver 4.2.2.2" > /etc/resolv.conf
